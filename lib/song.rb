55da8f374a6a61d8d78a1c8b7355761dc8aba959
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-14 19:12:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,13 @@
 class Song
   @@song_count = 0
-  @@
+  @@artist_array = []
   
   def initialize(name, artist, genre)
     @@song_count += 1
     @name = name
     @artist = artist
     @genre = genre
+    @@artist_array << @artist
   end
   
   
